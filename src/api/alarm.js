f70f8b84539841fc52bec43bf46a6f58d47778d8
zhuxia0xuan <960518788@qq.com>
2019-06-28 11:00:15 +0800

    111

--- src/api/alarm.js
+++ src/api/alarm.js
@@ -1,8 +1,44 @@
 import request from '@/utils/request'
 
-export function getAlarmInfo(data) {
+export function getAlarmInfo() {
   return request({
     url: '/func/web/getAlarmInfo',
     method: 'post'
+  })
+}
+export function saveAlarm(cardId, alarmContent, X, Y, data1) {
+  return request({
+    url: '/func/web/saveAlarmList',
+    method: 'POST',
+    data: {
+      cardId,
+      alarmContent,
+      X,
+      Y,
+      data1
+    }
+  })
+}
+export function editAlarm(alarmId, cardId, alarmContent, X, Y, data1) {
+  return request({
+    url: '/func/web/editAlarmList',
+    method: 'POST',
+    data: {
+      alarmId,
+      cardId,
+      alarmContent,
+      X,
+      Y,
+      data1
+    }
+  })
+}
+export function deleteAlarmList(alarmId) {
+  return request({
+    url: '/func/web/deleteAlarmList',
+    method: 'POST',
+    data: {
+      alarmId
+    }
   })
 }
--- src/views/alarmRecord/index.vue
+++ src/views/alarmRecord/index.vue
@@ -1,130 +1,225 @@
 <template>
   <div class="app-container">
-      <div class="block">
+    <el-header height="60px" style="background-color: rgb(238, 241, 246) ;text-align: left; font-size: 12px">
+      <div style="width: 35%; float: left;">
         开始时间：
         <el-date-picker v-model="createDate" type="datetime" value-format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期时间" :picker-options="pickerOptionsStart" style="margin-right: 10px;" @change="startTimeStatus" />
         至
         <el-date-picker v-model="overDate" type="datetime" value-format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期时间" :picker-options="pickerOptionsEnd" style="margin-left: 10px;" @change="endTimeStatus" />
       </div>
-      <!--表格-->
-      <!--<el-table-->
-        <!--v-loading="listLoading"-->
-        <!--:data="list"-->
-        <!--element-loading-text="Loading"-->
-        <!--border-->
-        <!--fit-->
-        <!--highlight-current-row-->
-      <!--&gt;-->
-        <el-table
-                  :data="tableList.slice((currentPage-1)*pageSize,currentPage*pageSize)"
-                  strip
-                  v-loading="listLoading"
-                  element-loading-text="Loading"
-                  border
-                  fit
-                  highlight-current-row
-                  style="margin-top:10px">
-        <el-table-column
-          label="编号"
-          sortable
-          align="center"
-          min-width="160"
-        >
-          <template slot-scope="scope">
-            {{ scope.$index+1 }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="警告编号"
-          sortable
-          align="center"
-          min-width="160"
-        >
-          <template slot-scope="scope">
-            {{ scope.row.alarmId }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="卡片编号"
-          sortable
-          align="center"
-          min-width="160"
-        >
-          <template slot-scope="scope">
-            {{ scope.row.cardId }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="警告详情"
-          sortable
-          align="center"
-          min-width="160"
-        >
-          <template slot-scope="scope">
-            {{ scope.row.alarmContent }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="警告时间"
-          sortable
-          align="center"
-          min-width="160"
-        >
-          <template slot-scope="scope">
-            {{ scope.row.alarmTime }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="X轴坐标"
-          sortable
-          align="center"
-          min-width="160"
-        >
-          <template slot-scope="scope">
-            {{ scope.row.X }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="Y轴坐标"
-          sortable
-          align="center"
-          min-width="160"
-        >
-          <template slot-scope="scope">
-            {{ scope.row.Y }}
-          </template>
-        </el-table-column>
-      </el-table>
-      <!--分页-->
+      <div style="width: 10%;float: left">
+        <el-button type="primary" round @click="openSave()">增加</el-button>
+      </div>
+    </el-header>
+
+    <el-table
+      v-loading="listLoading"
+      :data="tableList.slice((currentPage-1)*pageSize,currentPage*pageSize)"
+      strip
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+      style="margin-top:10px"
+    >
+      <el-table-column
+        label="警告编号"
+        sortable
+        align="center"
+        min-width="160"
+      >
+        <template slot-scope="scope">
+          {{ scope.row.alarmId }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="卡片编号"
+        sortable
+        align="center"
+        min-width="160"
+      >
+        <template slot-scope="scope">
+          {{ scope.row.cardId }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="警告详情"
+        sortable
+        align="center"
+        min-width="160"
+      >
+        <template slot-scope="scope">
+          {{ scope.row.alarmContent }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="警告时间"
+        sortable
+        align="center"
+        min-width="160"
+      >
+        <template slot-scope="scope">
+          {{ scope.row.alarmTime }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="X轴坐标"
+        sortable
+        align="center"
+        min-width="160"
+      >
+        <template slot-scope="scope">
+          {{ scope.row.X }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="Y轴坐标"
+        sortable
+        align="center"
+        min-width="160"
+      >
+        <template slot-scope="scope">
+          {{ scope.row.Y }}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" min-width="160" align="center">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="edit(scope.row)">编辑</el-button>
+          <el-button size="mini" @click="deleteContent(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--编辑-->
+    <div>
+      <el-dialog title="编辑" :visible.sync="dialogFormVisible" width="500px">
+        <el-form ref="editlist" :model="editlist" :rules="rules" label-position="left" label-width="140px" style="width: 430px; margin-left:50px;">
+          <el-form-item label="警告编号:">
+            <el-input v-model="editlist.alarmId" readonly="true" style="width: 80%" disabled="disabled" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="卡片编号:" prop="cardId">
+            <el-input v-model="editlist.cardId" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="警告详情:" prop="alarmContent">
+            <el-input v-model="editlist.alarmContent" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="警告时间:" prop="date1">
+            <el-date-picker
+              type="datetime"
+              format="yyyy-MM-dd HH:mm:ss"
+              value-format="yyyy-MM-dd HH:mm:ss"
+              placeholder="选择时间"
+              v-model="editlist.date1"
+              style="width: 80%;"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="X轴坐标:" prop="X">
+            <el-input v-model="editlist.X" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="Y轴坐标:" prop="Y">
+            <el-input v-model="editlist.Y" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="edit1('editlist')">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
+    <!--保存-->
+    <div>
+      <el-dialog title="保存" :visible.sync="dialogSaveFormVisible" width="500px">
+        <el-form ref="save" :model="save" :rules="rules" label-position="left" label-width="140px" style="width: 430px; margin-left:50px;">
+          <el-form-item label="卡片编号:" prop="cardId">
+            <el-input v-model="save.cardId" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="警告详情:" prop="alarmContent">
+            <el-input v-model="save.alarmContent" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="警告时间:" prop="date1">
+            <el-date-picker
+              type="datetime"
+              format="yyyy-MM-dd HH:mm:ss"
+              value-format="yyyy-MM-dd HH:mm:ss"
+              placeholder="选择时间"
+              v-model="save.date1"
+              style="width: 80%;"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="X轴坐标:" prop="X">
+            <el-input v-model="save.X" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="Y轴坐标:" prop="Y">
+            <el-input v-model="save.Y" style="width: 80%" autocomplete="off" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogSaveFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="saveAlarm('save')">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
+
+    <!--分页-->
     <div class="block" style="float: right">
-    <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="currentPage"
-      :page-sizes="[10, 20, 50, 100]"
-      :page-size="pageSize"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="tableList.length">
-    </el-pagination>
+      <el-pagination
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 50, 100]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="tableList.length"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
 
 <script>
 
-import { getAlarmInfo } from '../../api/alarm'
+import { getAlarmInfo, saveAlarm, editAlarm, deleteAlarmList } from '../../api/alarm'
 
 export default {
   name: 'AlarmRecord',
-  filters: {
-
-  },
+  filters: {},
   data() {
     return {
       pageSize: 10, // 每页的数据条数
       currentPage: 1, // 默认开始页面
       data: [],
       list: [],
+      dialogFormVisible: false,
+      dialogSaveFormVisible: false,
+      editlist: {
+        alarmId: '',
+        cardId: '',
+        alarmContent: '',
+        X: '',
+        Y: '',
+        date1: ''
+      },
+      save: {
+        cardId: '',
+        alarmContent: '',
+        X: '',
+        Y: '',
+        date1: '',
+        date2: ''
+      },
+      rules: {
+        cardId: [
+          { required: true, message: '请输入卡片编号', trigger: 'blur' }
+        ],
+        alarmContent: [
+          { required: true, message: '请输入警告内容', trigger: 'blur' }
+        ],
+        X: [
+          { required: true, message: '请输入X轴坐标', trigger: 'blur' }
+        ],
+        Y: [
+          { required: true, message: '请输入Y轴坐标', trigger: 'blur' }
+        ],
+        date1: [
+          { required: true, message: '请选择日期', trigger: 'change' }
+        ]
+      },
       listLoading: true,
       createDate: '',
       overDate: '',
@@ -158,7 +253,11 @@ export default {
         if (!this.createDate || !this.overDate) {
           return true
         }
-        if (item.alarmTime > this.createDate && item.alarmTime < this.overDate) { return true } else { return false }
+        if (item.alarmTime > this.createDate && item.alarmTime < this.overDate) {
+          return true
+        } else {
+          return false
+        }
       })
     }
   },
@@ -167,6 +266,121 @@ export default {
   },
   methods: {
 
+    deleteContent(item) {
+      this.editlist = item
+      this.$confirm('此操作将永久删除该信息, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        deleteAlarmList(this.editlist.alarmId).then(res => {
+          if (res.re === 1) {
+            this.$message({
+              message: '删除成功',
+              type: 'success'
+            })
+            this.fetchData()
+          } else {
+            this.$message.error('删除失败')
+          }
+        }).catch(e => {
+
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    edit(item) {
+      this.editlist = item
+      this.dialogFormVisible = true
+    },
+    edit1(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.dialogFormVisible = false
+          editAlarm(this.editlist.alarmId, this.editlist.cardId,
+            this.editlist.alarmContent, this.editlist.X
+            , this.editlist.Y, this.editlist.date1).then(res => {
+            if (res.re === 1) {
+              this.$message({
+                message: '更新成功',
+                type: 'success'
+              })
+            } else {
+              this.$message.error('更新失败')
+            }
+          }).catch(e => {
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+      // this.dialogFormVisible = false
+      // editAlarm(this.editlist.alarmId, this.editlist.cardId,
+      //   this.editlist.alarmContent, this.editlist.X
+      //   , this.editlist.Y).then(res => {
+      //   if (res.re === 1) {
+      //     this.$message({
+      //       message: '更新成功',
+      //       type: 'success'
+      //     })
+      //   } else {
+      //     this.$message.error('更新失败')
+      //   }
+      // }).catch(e => {
+      //
+      // })
+    },
+    saveAlarm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.dialogSaveFormVisible = false
+          saveAlarm(this.save.cardId,
+            this.save.alarmContent, this.save.X
+            , this.save.Y, this.save.date1).then(res => {
+            console.log(res)
+            if (res.re === 1) {
+              this.$refs[formName].resetFields()
+              this.$message({
+                message: '保存成功',
+                type: 'success'
+              })
+            } else {
+              this.$message.error('保存失败')
+            }
+            this.fetchData()
+          }).catch(e => {
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+      // this.dialogSaveFormVisible = false
+      // saveAlarm(this.save.cardId,
+      //   this.save.alarmContent, this.save.X
+      //   , this.save.Y).then(res => {
+      //   console.log(res)
+      //   if (res.re === 1) {
+      //     this.$message({
+      //       message: '保存成功',
+      //       type: 'success'
+      //     })
+      //   } else {
+      //     this.$message.error('保存失败')
+      //   }
+      //   this.fetchData()
+      // }).catch(e => {
+      //
+      // })
+    },
+    openSave() {
+      this.dialogSaveFormVisible = true
+    },
     fetchData() {
       this.listLoading = true
       getAlarmInfo().then(response => {
@@ -193,10 +407,8 @@ export default {
     handleCurrentChange: function(currentPage) {
       this.currentPage = currentPage
     }
-
   }
 }
-
 </script>
 <style>
 </style>
